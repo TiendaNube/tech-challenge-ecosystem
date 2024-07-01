fba5a62d279353426764e9fe37f9efb218a06804
Lucas Pernas <lucas.gabriel.pf@gmail.com>
2024-07-01 00:15:20 -0300

    Adding base project

---
+++ TechChallengeApplication/Controllers/TransactionAuthorizationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechChallenge.Core.Services;
+namespace TechChallengeApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionAuthorizationController : ControllerBase
+    {
+        private readonly TransactionAuthorizationService _authorizationService;
+
+        public TransactionAuthorizationController(TransactionAuthorizationService service)
+        {
+            _authorizationService = service;
+        }
+    }
+}
---
+++ TechChallengeApplication/Models/AuthorizationRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechChallengeApplication.Models
+{
+    public class AuthorizationRequest
+    {
+        [Required]
+        public string MerchantId { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string PaymentMethod { get; set; } = string.Empty;
+        [Required]
+        [CreditCard]
+        public string CardNumber { get; set; } = string.Empty;
+        [Required]
+        public string CardHolderName { get; set; } = string.Empty;
+        [Required]
+        public DateTime CardExpirationDate { get; set; }
+        [Required]
+        [StringLength(4, MinimumLength = 3)]
+        public string CardCVV { get; set; } = string.Empty;
+    }
+}
