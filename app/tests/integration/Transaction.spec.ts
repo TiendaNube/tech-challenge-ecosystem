db26c67ad8d4a25b24f3b6628a415faef925d346
Dayvson Sales <dayvson@outlook.com>
2024-08-15 13:32:27 -0300

    Add transaction integration test
    Signed-off-by: Dayvson Sales <dayvson@outlook.com>

---
+++ app/tests/integration/Transaction.spec.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { app } from '../../src/infra/http/server';
+import prisma from '../setup';
+
+describe('Transaction API', () => {
+  let merchantId: number;
+
+  beforeAll(async () => {
+    // Cria um merchant para associar às transações
+    const merchant = await prisma.merchant.create({
+      data: {
+        name: 'Test Merchant',
+      },
+    });
+    merchantId = merchant.id;
+  });
+
+  afterAll(async () => {
+    await prisma.transaction.deleteMany();
+    await prisma.payable.deleteMany();
+    await prisma.merchant.deleteMany();
+  });
+
+  it('should create a new transaction', async () => {
+    const transactionData = {
+      description: 'Test transaction',
+      payment_method: 'credit_card',
+      card_number: '1234567812345678',
+      card_holder: 'John Doe',
+      cvv: '123',
+      expiration_date: '02/2024',
+      merchant_id: merchantId,
+      total: 100.0,
+    };
+
+    const res = await request(app).post('/transaction').send(transactionData);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('merchant');
+    expect(res.body.description).toBe(transactionData.description);
+    expect(res.body.card_number).toBe(
+      transactionData.card_number.substring(
+        transactionData.card_number.length - 4,
+      ),
+    );
+  });
+});
